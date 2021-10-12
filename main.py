08472f51fe2672545326f76523701cc6c6a1ab82
Prateek1009 <59309919+Prateek1009@users.noreply.github.com>
2021-10-12 10:02:42 +0530

    updated function call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-This is the Main module. See use case
+This is the Main module.
 """
 
 
@@ -7,46 +7,42 @@ def take_inputs():
     """
     defines the use input
     Returns:
-        ALGORITHM (int): algorithm type. 0 for RAPTOR and 1 for TBTR
-        VARIANT (int): variant of the algorithm. 0 for normal version, 1 for range version, 2 for One-To-Many version, 3 for Hyper version
+        algorithm (int): algorithm type. 0 for RAPTOR and 1 for TBTR
+        variant (int): variant of the algorithm. 0 for normal version, 1 for range version, 2 for One-To-Many version, 3 for Hyper version
     """
-    ALGORITHM = int(input("Press 0 for RAPTOR  \nPress 1 for TBTR\n"))
+    algorithm = int(input("Press 0 for RAPTOR  \nPress 1 for TBTR\n"))
     print("***************")
-    if ALGORITHM == 0:
-        VARIANT = int(input(
-            "Press 0 for Standard RAPTOR\nPress 1 for rRAPTOR  \nPress 2 for One-To-Many rRAPTOR \nPress 3 for HypRAPTOR\n"))
-    elif ALGORITHM == 1:
-        VARIANT = int(
-            input("Press 0: Standard TBTR\n Press 1: rTBTR  \nPress 2: One-To-Many rTBTR \nPress 3: HypTBTR\n"))
+    if algorithm == 0:
+        variant = int(input("Press 0 for RAPTOR\nPress 1 for rRAPTOR  \nPress 2 for One-To-Many rRAPTOR \nPress 3 for HypRAPTOR\n"))
+    elif algorithm == 1:
+        variant = int(input("Press 0: TBTR\n Press 1: rTBTR  \nPress 2: One-To-Many rTBTR \nPress 3: HypTBTR\n"))
     print("***************")
-    return ALGORITHM, VARIANT
+    return algorithm, variant
 
 
 def main():
     """
-    Runs the test case depending upon the values of ALGORITHM, VARIANT
-    Args:
-    Returns: None
+    Runs the test case depending upon the values of algorithm, variant
     """
-    ALGORITHM, VARIANT = take_inputs()
-    if ALGORITHM == 0:
-        if VARIANT == 0:
-            pass
-        elif VARIANT == 1:
-            pass
-        elif VARIANT == 2:
-            pass
-        elif VARIANT == 3:
-            pass
-    if ALGORITHM == 1:
-        if VARIANT == 0:
-            pass
-        elif VARIANT == 1:
-            pass
-        elif VARIANT == 2:
-            pass
-        elif VARIANT == 3:
-            pass
+    algorithm, variant = take_inputs()
+    if algorithm == 0:
+        if variant == 0:
+            import test_case.std_raptor_tc
+        elif variant == 1:
+            import test_case.rraptor_tc
+        elif variant == 2:
+            import test_case.one_to_many_rraptor
+        elif variant == 3:
+            import test_case.hypraptor_tc
+    if algorithm == 1:
+        if variant == 0:
+            import test_case.std_tbtr_tc
+        elif variant == 1:
+            import test_case.rtbtr_tc
+        elif variant == 2:
+            import test_case.one_to_many_rtbtr_tc
+        elif variant == 3:
+            import test_case.hyptbtr_tc
 
 
 if __name__ == "__main__":
