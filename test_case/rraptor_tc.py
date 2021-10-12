3031aa79159a66c769c07bd3d56207c70a816ba3
Prateek1009 <59309919+Prateek1009@users.noreply.github.com>
2021-10-12 10:01:00 +0530

    minor edits

--- test_case/rraptor_tc.py
+++ test_case/rraptor_tc.py
@@ -1,5 +1,5 @@
 """
-Module contains the test case for standard rRAPTOR implementation
+Module contains the test case for rRAPTOR implementation
 """
 from time import process_time as time_measure
 
@@ -28,7 +28,7 @@ MAX_TRANSFER = 4
 WALKING_FROM_SOURCE = 0
 CHANGE_TIME_SEC = 0
 PRINT_PARA = 1
-OPTIMIZED = 0
+OPTIMIZED = 1
 D_TIME = -1
 print_query_parameters(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE)
 ########################################
@@ -36,8 +36,7 @@ d_time_groups = stop_times_file.groupby("stop_id")
 print("Optimal journeys are:\n###################################")
 start = time_measure()
 output = rraptor(SOURCE, DESTINATION, d_time_groups, MAX_TRANSFER, WALKING_FROM_SOURCE, CHANGE_TIME_SEC, PRINT_PARA,
-                 OPTIMIZED,
-                 routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict)
+                 OPTIMIZED, routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict)
 if OPTIMIZED == 1:
     print(f"Trips required to cover optimal journeys are {output}")
 else:
