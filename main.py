76ad44b3822db1bbd83f757640c9ce844acd8d26
Prateek1009 <59309919+Prateek1009@users.noreply.github.com>
2021-10-14 13:08:13 +0530

    added test cases in main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 """
 This is the Main module.
 """
+from collections import defaultdict
+
+from RAPTOR.std_raptor import raptor
+from RAPTOR.rraptor import rraptor
+from RAPTOR.one_to_many_rraptor import onetomany_rraptor
+from RAPTOR.hypraptor import hypraptor
+from TBTR.tbtr import tbtr
+from TBTR.rtbtr import rtbtr
+from TBTR.one_many_tbtr import onetomany_rtbtr
+from TBTR.hyptbtr import hyptbtr
+from miscellaneous_func import *
+
+print_logo()
+print("Reading Testcase...")
 
 
 def take_inputs():
@@ -29,25 +43,82 @@ def main():
     Runs the test case depending upon the values of algorithm, variant
     """
     algorithm, variant = take_inputs()
+    print_query_parameters(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, variant, NO_OF_PARTITIONS=4,
+                           WEIGHING_SCHEME="S2", PARTITIONING_ALGORITHM="KaHyPar")
     if algorithm == 0:
         if variant == 0:
-            import test_case.std_raptor_tc
+            output = raptor(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, CHANGE_TIME_SEC, PRINT_PARA,
+                            routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict)
+            print(f"Optimal arrival time are: {output}")
         elif variant == 1:
-            import test_case.rraptor_tc
+            output = rraptor(SOURCE, DESTINATION, d_time_groups, MAX_TRANSFER, WALKING_FROM_SOURCE, CHANGE_TIME_SEC, PRINT_PARA,
+                             OPTIMIZED, routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict)
+            if OPTIMIZED == 1:
+                print(f"Trips required to cover optimal journeys are {output}")
+            else:
+                print(f"Routes required to cover optimal journeys are {output}")
         elif variant == 2:
-            import test_case.one_to_many_rraptor
+            output = onetomany_rraptor(SOURCE, DESTINATION_LIST, d_time_groups, MAX_TRANSFER, WALKING_FROM_SOURCE, CHANGE_TIME_SEC,
+                                       PRINT_PARA, OPTIMIZED,
+                                       routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict)
+            if OPTIMIZED == 1:
+                print(f"Trips required to cover optimal journeys are {output}")
+            else:
+                print(f"Routes required to cover optimal journeys are {output}")
         elif variant == 3:
-            import test_case.hypraptor_tc
+            output = hypraptor(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, CHANGE_TIME_SEC, PRINT_PARA,
+                               stop_out, route_groups, routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict)
+            print(f"Optimal arrival time are: {output}")
     if algorithm == 1:
         if variant == 0:
-            import test_case.std_tbtr_tc
+            output = tbtr(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, PRINT_PARA, routes_by_stop_dict, stops_dict, stoptimes_dict,
+                          footpath_dict, trip_transfer_dict, trip_set)
+            print(f"Optimal arrival times are: {output[0]}")
         elif variant == 1:
-            import test_case.rtbtr_tc
+            output = rtbtr(SOURCE, DESTINATION, d_time_groups, MAX_TRANSFER, WALKING_FROM_SOURCE, PRINT_PARA, OPTIMIZED,
+                           routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict, trip_transfer_dict, trip_set)
+            if OPTIMIZED == 1:
+                print(f"Trips required to cover optimal journeys are {output}")
+            else:
+                print(f"Routes required to cover optimal journeys are {output}")
         elif variant == 2:
-            import test_case.one_to_many_rtbtr_tc
+            output = onetomany_rtbtr(SOURCE, DESTINATION_LIST, d_time_groups, MAX_TRANSFER, WALKING_FROM_SOURCE, PRINT_PARA,
+                                     OPTIMIZED, routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict, trip_transfer_dict, trip_set)
+            if OPTIMIZED == 1:
+                print(f"Trips required to cover optimal journeys are {output}")
+            else:
+                print(f"Routes required to cover optimal journeys are {output}")
         elif variant == 3:
-            import test_case.hyptbtr_tc
+            output = hyptbtr(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, PRINT_PARA, stop_out, trip_groups,
+                             routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict, trip_transfer_dict, trip_set)
+            print(f"Optimal arrival times are: {output[0]}")
 
 
 if __name__ == "__main__":
+    # Read network
+    FOLDER = './swiss'
+    stops_file, trips_file, stop_times_file, transfers_file, stops_dict, stoptimes_dict, footpath_dict, routes_by_stop_dict = read_testcase()
+    with open(f'./GTFS/{FOLDER}/TBTR_trip_transfer_dict.pkl', 'rb') as file:
+        trip_transfer_dict = pickle.load(file)
+    trip_set = set(trip_transfer_dict.keys())
+    for tid, connnections in trip_transfer_dict.items():
+        deaf = defaultdict(lambda: [])
+        deaf.update(connnections)
+        trip_transfer_dict[tid] = deaf
+    print_network_details(transfers_file, trips_file, stops_file)
+
+    # Query parameters
+    SOURCE = 9865
+    DESTINATION = 12683
+    DESTINATION_LIST = [12683]
+    D_TIME = stop_times_file.arrival_time.sort_values().iloc[0]
+    MAX_TRANSFER = 4
+    WALKING_FROM_SOURCE = 0
+    CHANGE_TIME_SEC = 0
+    PRINT_PARA = 0
+    OPTIMIZED = 1
+    stop_out, route_groups, _, trip_groups = read_partitions_new(stop_times_file, FOLDER, no_of_partitions=4, weighting_scheme="S2", partitioning_algorithm="kahypar")
+
+    # main function
+    d_time_groups = stop_times_file.groupby("stop_id")
     main()
