eca1ba05a4ace404135f211b4c2a33bc18351fe2
Prateek1009 <59309919+Prateek1009@users.noreply.github.com>
2021-10-15 12:35:00 +0530

    added stop_idx_in_route

--- gtfs_loader.py
+++ gtfs_loader.py
@@ -12,6 +12,7 @@ def load_all_dict(FOLDER):
         stoptimes_dict (dict): keys: route ID, values: list of trips in the increasing order of start time. Format-> dict[route_ID] = [trip_1, trip_2] where trip_1 = [(stop id, arrival time), (stop id, arrival time)]
         footpath_dict (dict): preprocessed dict. Format {from_stop_id: [(to_stop_id, footpath_time)]}.
         routes_by_stop_dict (dict): preprocessed dict. Format {stop_id: [id of routes passing through stop]}.
+        idx_by_route_stop_dict (dict): preprocessed dict. Format {(route id, stop id): stop index in route}.
     """
     import pickle
     with open(f'./dict_builder/{FOLDER}/stops_dict_pkl.pkl', 'rb') as file:
@@ -21,8 +22,10 @@ def load_all_dict(FOLDER):
     with open(f'./dict_builder/{FOLDER}/transfers_dict_full.pkl', 'rb') as file:
         footpath_dict = pickle.load(file)
     with open(f'./dict_builder/{FOLDER}/routes_by_stop.pkl', 'rb') as file:
-        routes_by_stop = pickle.load(file)
-    return stops_dict, stoptimes_dict, footpath_dict, routes_by_stop
+        routes_by_stop_dict = pickle.load(file)
+    with open(f'./dict_builder/{FOLDER}/idx_by_route_stop.pkl', 'rb') as file:
+        idx_by_route_stop_dict = pickle.load(file)
+    return stops_dict, stoptimes_dict, footpath_dict, routes_by_stop_dict, idx_by_route_stop_dict
 
 
 def load_all_db(FOLDER):
@@ -30,17 +33,17 @@ def load_all_db(FOLDER):
     Args:
         FOLDER (str): path to network folder.
     Returns:
-        stops (pandas.dataframe): dataframe with stop details.
-        trips (pandas.dataframe): dataframe with trip details.
-        stop_times (pandas.dataframe): dataframe with stoptimes details.
-        transfers (pandas.dataframe): dataframe with transfers (footpath) details.
+        stops_file (pandas.dataframe): dataframe with stop details.
+        trips_file (pandas.dataframe): dataframe with trip details.
+        stop_times_file (pandas.dataframe): dataframe with stoptimes details.
+        transfers_file (pandas.dataframe): dataframe with transfers (footpath) details.
     """
     import pandas as pd
     path = f"./GTFS/{FOLDER}"
-    stops = pd.read_csv(f'{path}/stops.txt', sep=',').sort_values(by=['stop_id']).reset_index(drop=True)
-    trips = pd.read_csv(f'{path}/trips.txt', sep=',')
-    stop_times = pd.read_csv(f'{path}/stop_times.csv', sep=',')
-    stop_times.arrival_time = pd.to_datetime(stop_times.arrival_time)
-    stop_times = pd.merge(stop_times, trips, on='trip_id')
-    transfers = pd.read_csv(f'{path}/transfers_full.csv', sep=',')
-    return stops, trips, stop_times, transfers
+    stops_file = pd.read_csv(f'{path}/stops.txt', sep=',').sort_values(by=['stop_id']).reset_index(drop=True)
+    trips_file = pd.read_csv(f'{path}/trips.txt', sep=',')
+    stop_times_file = pd.read_csv(f'{path}/stop_times.csv', sep=',')
+    stop_times_file.arrival_time = pd.to_datetime(stop_times_file.arrival_time)
+    stop_times_file = pd.merge(stop_times_file, trips_file, on='trip_id')
+    transfers_file = pd.read_csv(f'{path}/transfers_full.csv', sep=',')
+    return stops_file, trips_file, stop_times_file, transfers_file
