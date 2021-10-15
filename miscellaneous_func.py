7c50601403d1596d4e2dc8c0771de54c641d59c7
Prateek1009 <59309919+Prateek1009@users.noreply.github.com>
2021-10-15 12:35:24 +0530

    added docstring in readtest case function

--- miscellaneous_func.py
+++ miscellaneous_func.py
@@ -7,18 +7,32 @@ import networkx as nx
 import pandas as pd
 
 def read_testcase():
+    """
+    Reads the GTFS network and preprocessed dict. If the dicts are not present, dict_builder_functions are called to construct them.
+    Returns:
+        stops_file (pandas.dataframe):  stops.txt file in GTFS.
+        trips_file (pandas.dataframe): trips.txt file in GTFS.
+        stop_times_file (pandas.dataframe): stop_times.txt file in GTFS.
+        transfers_file (pandas.dataframe): dataframe with transfers (footpath) details.
+        stops_dict (dict): keys: route_id, values: list of stop id in the route_id. Format-> dict[route_id] = [stop_id]
+        stoptimes_dict (dict): keys: route ID, values: list of trips in the increasing order of start time. Format-> dict[route_ID] = [trip_1, trip_2] where trip_1 = [(stop id, arrival time), (stop id, arrival time)]
+        footpath_dict (dict): keys: from stop_id, values: list of tuples of form (to stop id, footpath duration). Format-> dict[stop_id]=[(stop_id, footpath_duration)]
+        route_by_stop_dict_new (dict): keys: stop_id, values: list of routes passing through the stop_id. Format-> dict[stop_id] = [route_id]
+        idx_by_route_stop_dict (dict): preprocessed dict. Format {(route id, stop id): stop index in route}.
+    """
     import gtfs_loader
     from dict_builder import dict_builder_functions
     FOLDER = './swiss'
     stops_file, trips_file, stop_times_file, transfers_file = gtfs_loader.load_all_db(FOLDER)
     try:
-        stops_dict, stoptimes_dict, footpath_dict, routes_by_stop_dict = gtfs_loader.load_all_dict(FOLDER)
+        stops_dict, stoptimes_dict, footpath_dict, routes_by_stop_dict, idx_by_route_stop_dict = gtfs_loader.load_all_dict(FOLDER)
     except FileNotFoundError:
         stops_dict = dict_builder_functions.build_save_stops_dict(stop_times_file, trips_file, FOLDER)
         stoptimes_dict = dict_builder_functions.build_save_stopstimes_dict(stop_times_file, trips_file, FOLDER)
         routes_by_stop_dict = dict_builder_functions.build_save_route_by_stop(stop_times_file, FOLDER)
         footpath_dict = dict_builder_functions.build_save_footpath_dict(transfers_file, FOLDER)
-    return stops_file, trips_file, stop_times_file, transfers_file, stops_dict, stoptimes_dict, footpath_dict, routes_by_stop_dict
+        idx_by_route_stop_dict = dict_builder_functions.stop_idx_in_route(stop_times_file, FOLDER)
+    return stops_file, trips_file, stop_times_file, transfers_file, stops_dict, stoptimes_dict, footpath_dict, routes_by_stop_dict, idx_by_route_stop_dict
 
 def print_logo():
     """
