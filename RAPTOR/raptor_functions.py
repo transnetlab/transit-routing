bebabe2b741d3a4ec75ee2ed5f062b7ec3ea07ba
Prateek1009 <59309919+Prateek1009@users.noreply.github.com>
2021-10-18 14:48:19 +0530

    removed pointer_label todo

--- RAPTOR/raptor_functions.py
+++ RAPTOR/raptor_functions.py
@@ -9,7 +9,7 @@ import pandas as pd
 ################################################################################################################################################################################################
 
 def initialize_raptor(routes_by_stop_dict, SOURCE, MAX_TRANSFER):
-    ''' # TODO: pi is to trace the path? what does the pointer point to?
+    '''
     Initialize values for RAPTOR.
     Args:
         routes_by_stop_dict (dict): preprocessed dict. Format {stop_id: [id of routes passing through stop]}.
@@ -18,7 +18,8 @@ def initialize_raptor(routes_by_stop_dict, SOURCE, MAX_TRANSFER):
     Returns:
         marked_stop (deque): deque to store marked stop.
         label (dict): nested dict to maintain label. Format {round : {stop_id: pandas.datetime}}.
-        pi_label (dict): Nested dict used for backtracking. Format {round : {stop_id: pointer_label}}
+        pi_label (dict): Nested dict used for backtracking labels. Format {round : {stop_id: pointer_label}}
+        if stop is reached by walking, pointer_label= ('walking', from stop id, to stop id, time, arrival time)}} else pointer_label= (trip boarding time, boarding_point, stop id, arr_by_trip, trip id)
         star_label (dict): dict to maintain best arrival label {stop id: pandas.datetime}.
         inf_time (pd.timestamp): Variable indicating infinite time (pandas.datetime).
     '''
