f498943534bcf59e88824b6e089c886827724afc
Prateek1009 <59309919+Prateek1009@users.noreply.github.com>
2021-10-19 13:08:55 +0530

    updated marked_stop and added pruning in footpath

--- RAPTOR/std_raptor.py
+++ RAPTOR/std_raptor.py
@@ -28,7 +28,7 @@ def raptor(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, CHANG
 
     out = []
     # Initialization
-    marked_stop, label, pi_label, star_label, inf_time = initialize_raptor(routes_by_stop_dict, SOURCE, MAX_TRANSFER)
+    marked_stop, marked_stop_dict, label, pi_label, star_label, inf_time = initialize_raptor(routes_by_stop_dict, SOURCE, MAX_TRANSFER)
     change_time = pd.to_timedelta(CHANGE_TIME_SEC, unit='seconds')
     (label[0][SOURCE], star_label[SOURCE]) = (D_TIME, D_TIME)
     Q = {}  # Format of Q is {route:stop index}
@@ -40,7 +40,9 @@ def raptor(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, CHANG
                 label[0][p_dash] = D_TIME + to_pdash_time
                 star_label[p_dash] = D_TIME + to_pdash_time
                 pi_label[0][p_dash] = ('walking', SOURCE, p_dash, to_pdash_time, D_TIME + to_pdash_time)
-                marked_stop.append(p_dash)
+                if marked_stop_dict[p_dash] == 0:
+                    marked_stop.append(p_dash)
+                    marked_stop_dict[p_dash] = 1
         except KeyError:
             pass
 
@@ -48,9 +50,9 @@ def raptor(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, CHANG
     # Main code part 1
     for k in range(1, MAX_TRANSFER + 1):
         Q.clear()
-        marked_stop = list(set(marked_stop))  # removes duplicates
         while marked_stop:
             p = marked_stop.pop()
+            marked_stop_dict[p] = 0
             try:
                 routes_serving_p = routes_by_stop_dict[p]
                 for route in routes_serving_p:
@@ -66,16 +68,16 @@ def raptor(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, CHANG
         for route, current_stopindex_by_route in Q.items():
             current_trip_t = -1
             for p_i in stops_dict[route][current_stopindex_by_route:]:
-                if current_trip_t != -1 and current_trip_t[current_stopindex_by_route][1] < min(star_label[p_i],
-                                                                                                star_label[
-                                                                                                    DESTINATION]):
+                if current_trip_t != -1 and current_trip_t[current_stopindex_by_route][1] < min(star_label[p_i], star_label[DESTINATION]):
                     arr_by_t_at_pi = current_trip_t[current_stopindex_by_route][1]
                     label[k][p_i], star_label[p_i] = arr_by_t_at_pi, arr_by_t_at_pi
                     pi_label[k][p_i] = (boarding_time, boarding_point, p_i, arr_by_t_at_pi, tid)
-                    marked_stop.append(p_i)  # TODO: you allow duplicates here and remove them later?
-                if current_trip_t == -1 or label[k - 1][p_i] + change_time < current_trip_t[current_stopindex_by_route][1]:  # assuming arrival_time = departure_time
-                    tid, current_trip_t = get_latest_trip_new(stoptimes_dict, route, label[k - 1][p_i],
-                                                              current_stopindex_by_route, change_time)
+                    if marked_stop_dict[p_i] == 0:
+                        marked_stop.append(p_i)
+                        marked_stop_dict[p_i] = 1
+                if current_trip_t == -1 or label[k - 1][p_i] + change_time < current_trip_t[current_stopindex_by_route][
+                    1]:  # assuming arrival_time = departure_time
+                    tid, current_trip_t = get_latest_trip_new(stoptimes_dict, route, label[k - 1][p_i], current_stopindex_by_route, change_time)
                     if current_trip_t == -1:
                         boarding_time, boarding_point = -1, -1
                     else:
@@ -91,13 +93,14 @@ def raptor(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, CHANG
                 for i in trans_info:
                     (p_dash, to_pdash_time) = i
                     new_p_dash_time = label[k][p] + to_pdash_time
-                    if label[k][p_dash] > new_p_dash_time:
+                    if label[k][p_dash] > new_p_dash_time and new_p_dash_time < min(star_label[p_dash], star_label[DESTINATION]):
                         label[k][p_dash], star_label[p_dash] = new_p_dash_time, new_p_dash_time
                         pi_label[k][p_dash] = ('walking', p, p_dash, to_pdash_time, new_p_dash_time)
-                        marked_stop.append(p_dash)
+                        if marked_stop_dict[p_dash] == 0:
+                            marked_stop.append(p_dash)
+                            marked_stop_dict[p_dash] = 1
             except KeyError:
                 continue
-
         # Main code End
         if marked_stop == deque([]):
             if PRINT_PARA == 1:
