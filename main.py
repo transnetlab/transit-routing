e59c8286bc12327bfecd6ce1faa0694e81eebf8c
Prateek1009 <59309919+Prateek1009@users.noreply.github.com>
2021-10-23 17:22:56 +0530

    PRINT_PARA changed to PRINT_ITINERARY and added nested functionality

--- main.py
+++ main.py
@@ -47,11 +47,11 @@ def main():
                            weighting_scheme="S2", partitioning_algorithm="KaHyPar")
     if algorithm == 0:
         if variant == 0:
-            output = raptor(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, CHANGE_TIME_SEC, PRINT_PARA,
+            output = raptor(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, CHANGE_TIME_SEC, PRINT_ITINERARY,
                             routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict, idx_by_route_stop_dict)
             print(f"Optimal arrival time are: {output}")
         elif variant == 1:
-            output = rraptor(SOURCE, DESTINATION, d_time_groups, MAX_TRANSFER, WALKING_FROM_SOURCE, CHANGE_TIME_SEC, PRINT_PARA,
+            output = rraptor(SOURCE, DESTINATION, d_time_groups, MAX_TRANSFER, WALKING_FROM_SOURCE, CHANGE_TIME_SEC, PRINT_ITINERARY,
                              OPTIMIZED, routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict, idx_by_route_stop_dict)
             if OPTIMIZED == 1:
                 print(f"Trips required to cover optimal journeys are {output}")
@@ -59,40 +59,47 @@ def main():
                 print(f"Routes required to cover optimal journeys are {output}")
         elif variant == 2:
             output = onetomany_rraptor(SOURCE, DESTINATION_LIST, d_time_groups, MAX_TRANSFER, WALKING_FROM_SOURCE, CHANGE_TIME_SEC,
-                                       PRINT_PARA, OPTIMIZED,
+                                       PRINT_ITINERARY, OPTIMIZED,
                                        routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict, idx_by_route_stop_dict)
             if OPTIMIZED == 1:
                 print(f"Trips required to cover optimal journeys are {output}")
             else:
                 print(f"Routes required to cover optimal journeys are {output}")
         elif variant == 3:
-            output = hypraptor(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, CHANGE_TIME_SEC, PRINT_PARA,
+            output = hypraptor(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, CHANGE_TIME_SEC, PRINT_ITINERARY,
                                stop_out, route_groups, routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict, idx_by_route_stop_dict)
+            print(f"Optimal arrival time are: {output}")
+        elif variant == 4:
+            output = hypraptor(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, CHANGE_TIME_SEC, PRINT_ITINERARY,
+                               nested_stop_out, nested_route_groups, routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict, idx_by_route_stop_dict)
             print(f"Optimal arrival time are: {output}")
     if algorithm == 1:
         if variant == 0:
-            output = tbtr(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, PRINT_PARA, routes_by_stop_dict, stops_dict, stoptimes_dict,
+            output = tbtr(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, PRINT_ITINERARY, routes_by_stop_dict, stops_dict, stoptimes_dict,
                           footpath_dict, idx_by_route_stop_dict, trip_transfer_dict, trip_set)
             print(f"Optimal arrival times are: {output[0]}")
         elif variant == 1:
-            output = rtbtr(SOURCE, DESTINATION, d_time_groups, MAX_TRANSFER, WALKING_FROM_SOURCE, PRINT_PARA, OPTIMIZED,
+            output = rtbtr(SOURCE, DESTINATION, d_time_groups, MAX_TRANSFER, WALKING_FROM_SOURCE, PRINT_ITINERARY, OPTIMIZED,
                            routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict, idx_by_route_stop_dict, trip_transfer_dict, trip_set)
             if OPTIMIZED == 1:
                 print(f"Trips required to cover optimal journeys are {output}")
             else:
                 print(f"Routes required to cover optimal journeys are {output}")
         elif variant == 2:
-            output = onetomany_rtbtr(SOURCE, DESTINATION_LIST, d_time_groups, MAX_TRANSFER, WALKING_FROM_SOURCE, PRINT_PARA,
+            output = onetomany_rtbtr(SOURCE, DESTINATION_LIST, d_time_groups, MAX_TRANSFER, WALKING_FROM_SOURCE, PRINT_ITINERARY,
                                      OPTIMIZED, routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict, idx_by_route_stop_dict, trip_transfer_dict, trip_set)
             if OPTIMIZED == 1:
                 print(f"Trips required to cover optimal journeys are {output}")
             else:
                 print(f"Routes required to cover optimal journeys are {output}")
         elif variant == 3:
-            output = hyptbtr(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, PRINT_PARA, stop_out, trip_groups,
+            output = hyptbtr(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, PRINT_ITINERARY, stop_out, trip_groups,
                              routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict, idx_by_route_stop_dict, trip_transfer_dict, trip_set)
             print(f"Optimal arrival times are: {output[0]}")
-
+        elif variant == 4:
+            output = hyptbtr(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, PRINT_ITINERARY, nested_stop_out, nested_trip_groups,
+                             routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict, idx_by_route_stop_dict, trip_transfer_dict, trip_set)
+            print(f"Optimal arrival times are: {output[0]}")
 
 if __name__ == "__main__":
     # Read network
@@ -113,9 +120,10 @@ if __name__ == "__main__":
     MAX_TRANSFER = 4
     WALKING_FROM_SOURCE = 1
     CHANGE_TIME_SEC = 0
-    PRINT_PARA = 0
+    PRINT_ITINERARY = 0
     OPTIMIZED = 1
     stop_out, route_groups, _, trip_groups = read_partitions_new(stop_times_file, FOLDER, no_of_partitions=4, weighting_scheme="S2", partitioning_algorithm="kahypar")
+    nested_stop_out, nested_route_groups, _, _, nested_trip_groups  = read_nested_partitions(stop_times_file, FOLDER, no_of_partitions=4, weighting_scheme="S2")
 
     # main function
     d_time_groups = stop_times_file.groupby("stop_id")
