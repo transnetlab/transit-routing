c04b3adc2662b3d6c570ed3f47905a3b1dcd0b6a
Prateek1009 <59309919+Prateek1009@users.noreply.github.com>
2021-10-23 16:53:26 +0530

    minor edits

--- TBTR/tbtr.py
+++ TBTR/tbtr.py
@@ -32,22 +32,22 @@ def tbtr(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, PRINT_P
     L = initialize_from_desti(routes_by_stop_dict, stops_dict, DESTINATION, footpath_dict, idx_by_route_stop_dict)
     R_t, Q = initialize_from_source(footpath_dict, SOURCE, routes_by_stop_dict, stops_dict, stoptimes_dict, D_TIME,
                                         MAX_TRANSFER, WALKING_FROM_SOURCE, idx_by_route_stop_dict)
-    n = 0
-    while n < MAX_TRANSFER:  # TODO: Pseudocdoe is n \leq \lambda?
-        for trip_segment in Q[n]:
-            from_stop, tid, to_stop, trip_route, tid_idx = trip_segment[0: 5]  # TODO: can you use a different name in 36 and 37 instead of trip so that it is not confused with 38
+    n = 1
+    while n <= MAX_TRANSFER:
+        for counter, trip_segment in enumerate(Q[n]):
+            from_stop, tid, to_stop, trip_route, tid_idx = trip_segment[0: 5]
             trip = stoptimes_dict[trip_route][tid_idx][from_stop:to_stop]
             try:
-                L[trip_route]  # TODO: what's this line doing? Checking if trip_route is in L?
+                L[trip_route]
                 stop_list, _ = zip(*trip)
                 for last_leg in L[trip_route]:
                     idx = [x[0] for x in enumerate(stop_list) if x[1] == last_leg[2]]
                     if idx and from_stop < last_leg[0] and trip[idx[0]][1] + last_leg[1] < J[n][0]:
-                        if last_leg[1] == 0:
-                            walking = (0, 0)  # TODO: Is this for tracking the path? I don't see it in the pseudocode
+                        if last_leg[1] == pd.to_timedelta(0, unit="seconds"):
+                            walking = (0, 0)
                         else:
                             walking = (1, stops_dict[trip_route][last_leg[0]])
-                        J = update_label(trip[idx[0]][1] + last_leg[1], n, (tid, walking), J, MAX_TRANSFER)
+                        J = update_label(trip[idx[0]][1] + last_leg[1], n, (tid, walking, counter), J, MAX_TRANSFER)
             except KeyError:
                 pass
             try:
@@ -55,11 +55,12 @@ def tbtr(SOURCE, DESTINATION, D_TIME, MAX_TRANSFER, WALKING_FROM_SOURCE, PRINT_P
                     connection_list = [connection for from_stop_idx, transfer_stop_id in
                                        enumerate(trip[1:], from_stop + 1)
                                        for connection in trip_transfer_dict[tid][from_stop_idx]]
-                    enqueue(connection_list, n + 1, (tid, 0, 0), R_t, Q, stoptimes_dict)
+                    enqueue(connection_list, n + 1, (tid, counter, 0), R_t, Q, stoptimes_dict)
             except IndexError:
                 pass
         n = n + 1
     TBTR_out = post_process(J, Q, DESTINATION, SOURCE, footpath_dict, stops_dict, stoptimes_dict, PRINT_PARA, D_TIME,
                             MAX_TRANSFER, idx_by_route_stop_dict)
     out.append(TBTR_out)
+
     return out
