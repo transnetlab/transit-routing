4b72d5eaf6289fc01e315f9217924a4d38d7912e
Prateek1009 <59309919+Prateek1009@users.noreply.github.com>
2021-10-23 17:19:11 +0530

    PRINT_PARA changed to PRINT_ITINERARY

--- TBTR/one_many_tbtr.py
+++ TBTR/one_many_tbtr.py
@@ -5,7 +5,7 @@ Module contains One-To-Many rTBTR implementation
 from TBTR.TBTR_functions import *
 
 
-def onetomany_rtbtr(SOURCE, DESTINATION_LIST, d_time_groups, MAX_TRANSFER, WALKING_FROM_SOURCE, PRINT_PARA, OPTIMIZED,
+def onetomany_rtbtr(SOURCE, DESTINATION_LIST, d_time_groups, MAX_TRANSFER, WALKING_FROM_SOURCE, PRINT_ITINERARY, OPTIMIZED,
                     routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict, idx_by_route_stop_dict, trip_transfer_dict, trip_set):
     """
     One to many rTBTR implementation
@@ -15,7 +15,7 @@ def onetomany_rtbtr(SOURCE, DESTINATION_LIST, d_time_groups, MAX_TRANSFER, WALKI
         d_time_groups (pandas.group): all possible departures times from all stops.
         MAX_TRANSFER (int): maximum transfer limit.
         WALKING_FROM_SOURCE (int): 1 or 0. 1 means walking from SOURCE is allowed.
-        PRINT_PARA (int): 1 or 0. 1 means print complete path.
+        PRINT_ITINERARY (int): 1 or 0. 1 means print complete path.
         OPTIMIZED (int): 1 or 0. 1 means collect trips and 0 means collect routes.
         routes_by_stop_dict (dict): preprocessed dict. Format {stop_id: [id of routes passing through stop]}.
         stops_dict (dict): preprocessed dict. Format {route_id: [ids of stops in the route]}.
