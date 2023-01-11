b5e0b92ee6c1d0b9306dfeb71e13868e9b46a043
Prateek1009 <59309919+Prateek1009@users.noreply.github.com>
2023-01-11 23:35:35 +0530

    Added functionality for building transfer patterns

--- main.py
+++ main.py
@@ -10,4 +10,5 @@ python_global_call = 'python'
 os.system(f'{python_global_call} GTFS_wrapper.py')
 os.system(f'{python_global_call} build_transfer_file.py')
 os.system(f'{python_global_call} build_TBTR_dict.py')
+os.system(f'{python_global_call} build_transfer_patterns.py')
 os.system(f'{python_global_call} query_file.py')
